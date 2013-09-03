bd80bbbe24c8a495b932b0bb19bb6772fb4985b2
glyphstone <breanna@glyphstone.net>
2013-09-03 11:05:19 -0700

    added more info on stub page

--- index.php
+++ index.php
@@ -1,6 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Dottify.me | Put Yourself on the Map</title>
+</head>
+<body>
 <?php
 $message = "Dottify yourself!\nPlease enter your country and zipcode below\n" ;
-
 echo $message ;
-
 ?>
+<h1>Welcome to Dottify.me</h1>
+This is a placeholder stub for Dottify.me social media research site for the Transgender/Trans* and Gender Variant communities.<br/>
+This application will be bootstrapped as part of the Trans*H4CK hackathon on September 13-15 2013.<br/>
+Our goal is to have a Minimum Viable Product (MVP) up and running by afternoon, Saturday 14 and ready for you to kick the tires.
+Please bookmark the site and watch here, our facebook page, follow @dottifyme #dottify for the announcement that we are up and 
+ready for you to dottify yourself.  Our goal is to reach over 100 dottified users by the demo time Sunday afternoon.
+<br/><br/>For more information on the project, please watch our blog site at <a href="http://dottify.org">Dottify.org</a>
+</body>
+</html>
