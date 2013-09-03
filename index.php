c70327154d058ae1ba7e2e13c492f0a5b161cf27
glyphstone <breanna@glyphstone.net>
2013-09-03 12:28:42 -0700

    move google tracking up to header

--- index.php
+++ index.php
@@ -3,7 +3,16 @@
 <head>
 <meta charset="UTF-8">
 <title>Dottify.me | Put Yourself on the Map</title>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
+  ga('create', 'UA-43726935-2', 'dottify.me');
+  ga('send', 'pageview');
+
+</script>
 </head>
 <body>
 <?php
@@ -17,15 +26,6 @@ Our goal is to have a Minimum Viable Product (MVP) up and running by afternoon, 
 Please bookmark the site and watch here, our facebook page, follow @dottifyme #dottify for the announcement that we are up and 
 ready for you to dottify yourself.  Our goal is to reach over 100 dottified users by the demo time Sunday afternoon.
 <br/><br/>For more information on the project, please watch our blog site at <a href="http://dottify.org">Dottify.org</a>
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-43726935-2', 'dottify.me');
-  ga('send', 'pageview');
-
-</script>
 </body>
 </html>
